d8418ea61ef4aa5bdc8ee12c282a4f6602ff9bf1
Ashutosh mishra <ashutoshmishra8796@gmail.com>
2025-05-26 15:40:45 +0530

    first commit

--- src/pages/contact/sections/MapLocation.jsx
+++ src/pages/contact/sections/MapLocation.jsx
@@ -10,7 +10,7 @@ const MapLocation = () => {
             <h2 className="section-title text-center mb-4">
               Find Us on the Map
             </h2>
-            <div className="map-container">
+            <div className="map-container flex flex-col">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.21557488971!2d-73.9878449240963!3d40.74844097138966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1690833879811!5m2!1sen!2sus"
                 width="100%"
